3917680a54874b37293829d890505dacfb5b0f2b
iamppz <iamppz@hotmail.com>
2020-11-03 22:16:45 +0800

    fix: build gh-pages

---
+++ build/precache-manifest.e24440dd2d8014c21c4ea172eb9fd924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d55f96426d8a675780026b09f0932cc",
+    "url": "/flowchart-react/index.html"
+  },
+  {
+    "revision": "b0ddcf9497aae7b85732",
+    "url": "/flowchart-react/static/css/2.cbe6aa73.chunk.css"
+  },
+  {
+    "revision": "49e560c24ecc9b595ffe",
+    "url": "/flowchart-react/static/css/main.442afe01.chunk.css"
+  },
+  {
+    "revision": "b0ddcf9497aae7b85732",
+    "url": "/flowchart-react/static/js/2.daee3743.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/flowchart-react/static/js/2.daee3743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e560c24ecc9b595ffe",
+    "url": "/flowchart-react/static/js/main.1be9b0e5.chunk.js"
+  },
+  {
+    "revision": "d5319b369e5c4464f0df",
+    "url": "/flowchart-react/static/js/runtime-main.a8752cf3.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e7c9943bb844f0582c4fb6034d7bf99.js"
+  "/flowchart-react/precache-manifest.e24440dd2d8014c21c4ea172eb9fd924.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flowchart-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
