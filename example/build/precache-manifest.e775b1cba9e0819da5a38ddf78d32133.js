2f9bc782a02174cd2580db8d4642d73a8aab84c9
iamppz <iamppz@hotmail.com>
2021-01-31 00:30:07 +0800

    chore: build gh-pages

---
+++ example/build/precache-manifest.e775b1cba9e0819da5a38ddf78d32133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beef46e77c9cb273be1089edeafed826",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79b26da03d7f877023f6",
+    "url": "./static/css/2.8c1dcbfa.chunk.css"
+  },
+  {
+    "revision": "c90a3fc08d92f3089fb4",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "79b26da03d7f877023f6",
+    "url": "./static/js/2.63ed8516.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.63ed8516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90a3fc08d92f3089fb4",
+    "url": "./static/js/main.b0a02e49.chunk.js"
+  },
+  {
+    "revision": "e1812f9786a9d0a009f8",
+    "url": "./static/js/runtime-main.ef37c694.js"
+  }
+]);
